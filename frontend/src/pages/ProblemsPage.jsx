9c0e02029e9a75eb70ab9a9186de136dc4102b86
Kavya <kavyabhanvadia@gmail.com>
2025-12-15 12:40:39 +0530

    Resolving errors

--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar.jsx";
 
 import { PROBLEMS } from "../data/problems";
 import { ChevronRightIcon, Code2Icon } from "lucide-react";
